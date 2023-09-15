8bf09f7a4fbfa270649b5c967f3f17f5f19ceea6
aurelienbono <bonombelleaurelien08@gmail.com>
2023-09-15 08:40:52 +0100

    adding new feature in doc

--- caesarian/views.py
+++ caesarian/views.py
@@ -9,6 +9,18 @@ def index(request) :
 	return render(request, 'pages/index.html')
 
 
+def login(request) : 
+	return render(request, 'pages/login.html')
+
+def register(request) : 
+	return render(request, 'pages/register.html')
+
+
+
+
+	
+
+
 def prediction(request) : 
 	user_data  = []
 	pregnant_woman = [] 
@@ -20,7 +32,7 @@ def prediction(request) :
 
 		# I retrieve user data on all data provided in the form
 		user_data = all_data[2:6]
-		
+
 		#saving userData to the database
 		user = models.UserData(user_name=user_data[0], phone_number_user=user_data[1], contry_user=user_data[2], gender_user=user_data[3])
 		user.save()
@@ -41,10 +53,3 @@ def prediction(request) :
 
 	return render(request, 'pages/prediction.html')
 
-	
-
-def login(request) : 
-	return render(request, 'pages/login.html')
-
-def register(request) : 
-	return render(request, 'pages/register.html')
